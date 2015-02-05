fe996c5293c127c5c1a3eb3a86388426d208c83f
Jeremiah Orem <oremj@oremj.com>
2015-02-04 17:36:45 -0800

    github/api: add basic auth

--- github/api_client.go
+++ github/api_client.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -13,11 +14,19 @@ type ApiClient struct {
 }
 
 func (c *ApiClient) Do(req *http.Request, v interface{}) error {
+	if c.Username != "" {
+		req.SetBasicAuth(c.Username, c.Password)
+	}
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("non-200 status code: %s", resp.Status)
+	}
 
 	return json.NewDecoder(resp.Body).Decode(v)
 }
