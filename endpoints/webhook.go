d26a8ae41e79921647e899c992e24e46f77e6664
Jeremiah Orem <oremj@oremj.com>
2015-02-04 16:08:03 -0800

    endpoints: get jobs to be updated

--- endpoints/webhook.go
+++ endpoints/webhook.go
@@ -1,9 +1,9 @@
 package endpoints
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/oremj/go-jenkins-webhook-proxy/github"
 	"github.com/oremj/go-jenkins-webhook-proxy/jenkins"
@@ -20,5 +20,26 @@ func (h *WebhookHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Print("HandleWebhook: ", err)
 		return
 	}
-	fmt.Println(payload.Ref)
+
+	if !payload.Created || strings.HasPrefix(payload.Ref, "refs/tags/") {
+		http.Error(w, "not a tag", http.StatusOK)
+		return
+	}
+
+	jobList, err := h.Jenkins.FetchJobList()
+	if err != nil {
+		http.Error(w, "could not parse payload", http.StatusBadRequest)
+		log.Print("HandleWebhook: ", err)
+		return
+	}
+
+	jobs := jobList.FilterByProperty("UpdateOnTag", payload.Repository.FullName)
+
+	for _, j := range jobs {
+		// TODO: build these jobs
+		println("Update: ", j.Name)
+		println("Tag: ", strings.TrimPrefix(payload.Ref, "refs/tags/"))
+		println("PuppetGitRef: ", "origin/master")
+		println("SvcopRef: ", "origin/master")
+	}
 }
